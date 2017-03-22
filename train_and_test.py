3fc00ab978223fa12eda5667531df56344c8b697
Brandon <budang@utexas.edu>
2017-03-22 11:33:13 -0500

    model testing code

--- train_and_test.py
+++ train_and_test.py
@@ -9,23 +9,27 @@ features_df = read_csv('./data/features.csv')
 train_df = features_df[0:60]
 test_df = features_df[60:90]
 
-train_X = train_df[:, 0:-1]	# feature vector
-train_y = train_df[:, -1]	# ground truth
+X_train = train_df[:, 0:-1]	# feature vector
+y_train = train_df[:, -1]	# ground truth
 
-test_X = test_df[:, 0:-1]	# feature vector
-test_y = test_df[:, -1]		# ground truth
+X_test = test_df[:, 0:-1]	# feature vector
+y_test = test_df[:, -1]		# ground truth
 
 # train
-svm_clf = svm.SVC()
-svm_clf.fit(train_X, train_y)
-
-tree_clf = tree.DecisionTreeClassifier()
-tree_clf.fit(train_X, train_y)
-
-lr_clf = LogisticRegression()
-lr_clf.fit(train_X, train_y)
-
-knn_clf = KNeighborsClassifier()
-knn_clf.fit(train_X, train_y)
+svm_clf = svm.SVC().fit(X_train, y_train)
+tree_clf = tree.DecisionTreeClassifier().fit(X_train, y_train)
+lr_clf = LogisticRegression().fit(X_train, y_train)
+knn_clf = KNeighborsClassifier().fit(X_train, y_train)
 
 # test
+svm_score = svm_clf.score(X_test, y_test)
+tree_score = tree_clf.score(X_test, y_test)
+lr_score = lr_clf.score(X_test, y_test)
+knn_score = knn_clf.score(X_test, y_test)
+
+# report accuracies
+print('accuracies')
+print('SupportVectorMachine:\t' + str(svm_score))
+print('DecisionTree:\t' + str(tree_score))
+print('LogisticRegression:\t' + str(lr_score))
+print('KNearestNeighbors:\t' + str(knn_score))
