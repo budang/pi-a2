49095b9bb46945d47958afbf10ffb5bd43e29e1e
Brandon <budang@utexas.edu>
2017-03-22 22:20:22 -0500

    shuffling training/testing sets and reporting precision/recall

--- train_and_test.py
+++ train_and_test.py
@@ -1,19 +1,22 @@
 # imports
+from pandas import read_csv, DataFrame
 from sklearn import svm, tree
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from pandas import read_csv, DataFrame
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.utils import shuffle
 
 # features
 features_df = read_csv('./data/features.csv')
-train = features_df[0:60].values
-test = features_df[60:90].values
+
+train = shuffle(features_df[0:60]).values
+test = shuffle(features_df[60:90]).values
 
 X_train = train[:, 0:-1]# feature vector
 y_train = train[:, -1]	# ground truth
 
 X_test = test[:, 0:-1]	# feature vector
-y_test = test[:, -1]	# ground truth
+y_test = test[:, -1]    # ground truth
 
 # train
 svm_clf = svm.SVC().fit(X_train, y_train)
@@ -22,6 +25,20 @@ lr_clf = LogisticRegression().fit(X_train, y_train)
 knn_clf = KNeighborsClassifier().fit(X_train, y_train)
 
 # test
+svm_predictions = svm_clf.predict(X_test)
+tree_predictions = tree_clf.predict(X_test)
+lr_predictions = lr_clf.predict(X_test)
+knn_predictions = knn_clf.predict(X_test)
+
+svm_p_r = precision_recall_fscore_support( \
+  y_test, svm_predictions, average='weighted')[:2]
+tree_p_r = precision_recall_fscore_support( \
+  y_test, tree_predictions, average='weighted')[:2]
+lr_p_r = precision_recall_fscore_support( \
+  y_test, lr_predictions, average='weighted')[:2]
+knn_p_r = precision_recall_fscore_support( \
+  y_test, knn_predictions, average='weighted')[:2]
+
 svm_score = svm_clf.score(X_test, y_test)
 tree_score = tree_clf.score(X_test, y_test)
 lr_score = lr_clf.score(X_test, y_test)
@@ -33,3 +50,12 @@ print('SupportVectorMachine:\t' + str(svm_score * 100) + '%')
 print('DecisionTree:\t\t' + str(tree_score * 100) + '%')
 print('LogisticRegression:\t' + str(lr_score * 100) + '%')
 print('KNearestNeighbors:\t' + str(knn_score * 100) + '%')
+print('')
+
+# report precision and recall
+print('PRECISION / RECALL')
+print('SupportVectorMachine:\t' + str(svm_p_r))
+print('DecisionTree:\t\t' + str(tree_p_r))
+print('LogisticRegression:\t' + str(lr_p_r))
+print('KNearestNeighbors:\t' + str(knn_p_r))
+print('')
